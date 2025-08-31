4943d57ac4ddee4a1b9efe09402a51c8b5041e0b
Jaco Bats <jacosbats@outlook.com>
2025-08-31 23:51:25 +0000

    is this why we cant even process the files metadata

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -5,15 +5,15 @@ import { getStorage, Storage } from "firebase-admin/storage";
 let adminApp: App;
 let adminStorage: Storage;
 
-const BUCKET_NAME = "cellular-data-streamer.appspot.com";
+// When deployed to App Hosting, the BUCKET_NAME is not needed
+// as the SDK will automatically use the project's default bucket.
+// const BUCKET_NAME = "cellular-data-streamer.appspot.com";
 
 try {
     if (!getApps().length) {
         // When deployed to App Hosting, the SDK will automatically use the
         // default service account credentials. This is the recommended approach.
-        adminApp = initializeApp({
-            storageBucket: BUCKET_NAME,
-        });
+        adminApp = initializeApp();
     } else {
         adminApp = getApps()[0];
     }
